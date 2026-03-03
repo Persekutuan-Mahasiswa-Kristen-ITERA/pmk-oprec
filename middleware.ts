97f464602ae379f87a94aa2bcae22538a136fb14
FYCode-dev12 <febrianyoel.contact@gmail.com>
2026-03-04 04:36:02 +0700

    chore: Update Supabase environment variable names to use `NEXT_PUBLIC_` prefix.

--- middleware.ts
+++ middleware.ts
@@ -8,8 +8,8 @@ export async function middleware(request: NextRequest) {
 
     // We need to create a Supabase client that can parse and set cookies
     const supabase = createServerClient(
-        process.env.SUPABASE_URL!,
-        process.env.SUPABASE_ANON_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
             cookies: {
                 getAll() {
